6b5784129e19348c071776e70eef99bb0f288ba5
Ivan <ivan.moreno.granado@gmail.com>
2022-11-15 17:44:00 +0100

    code(main): DISEÑO BD
    Creación de tablas referentes a los RI1-3

--- DataAnalytics/admin.py
+++ DataAnalytics/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from DataAnalytics.models import Piloto,Circuito,Constructor
 # Register your models here.
+
+@admin.register(Piloto)
+class PilotoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellidos', 'fecha_naciomiento', 'nacionalidad', 'abreviatura', 'enlace']
+
+@admin.register(Circuito)
+class CircuitoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'localizacion', 'pais', 'latitud', 'longitud', 'enlace']
+    
+@admin.register(Constructor)
+class ConstructorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'nacionalidad', 'enlace']
--- DataAnalytics/models.py
+++ DataAnalytics/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+# RI1: The system shall allow store the data of F1 pilots
+class Piloto(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.TextField(max_length=100)
+    fecha_naciomiento = models.DateTimeField()
+    nacionalidad = models.CharField(max_length=100)
+    abreviatura = models.DateTimeField(auto_now_add=True)
+    enlace = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+# RI2: The system shall allow store the data of F1 constrctors
+class Constructor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
+    enlace = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+# RI3: The system shall allow store the data of F1 circuits
+class Circuito(models.Model):
+    nombre = models.CharField(max_length=100)
+    localizacion = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    enlace = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
