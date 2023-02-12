1644d11407e4cfff4173508a2357ae6ebb44addb
Ivan <ivan.moreno.granado@gmail.com>
2023-02-12 10:34:25 +0100

    code(main): Integración de la API del tiempo en vista de detalles de carreras
    Se ha procesado los datos del json de respuesta de la API a la vista

--- DataAnalytics/meteo.py
+++ DataAnalytics/meteo.py
@@ -6,13 +6,14 @@ import json
 
 
 def get_weather(location, date, end_date,start_time,end_time):
+    meteo = []
     url = "https://visual-crossing-weather.p.rapidapi.com/history"
-
+    key = '\'' + location + '\''
     querystring = {"startDateTime":date,\
         "aggregateHours":"24","location":location,\
             "endDateTime":end_date,\
                 "unitGroup":"us","dayStartTime":start_time,\
-                    "contentType":"csv","dayEndTime":end_time,\
+                    "contentType":"json","dayEndTime":end_time,\
                         "shortColumnNames":"0"}
 
     headers = {
@@ -21,7 +22,23 @@ def get_weather(location, date, end_date,start_time,end_time):
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    
-    print(response.text)
-    return end_time
+    if response.status_code == 200:
+    # Cargue los datos devueltos en un objeto de Python
+        
+        lista_valores = list(response.json().values())
+        data_dict = lista_valores[-1]
+        data_parsed = list(data_dict.values())[0]['values'][0]
+        temperatura = data_parsed['temp']
+        print (temperatura)
+        precipitacion = data_parsed['precip']
+        humedad = data_parsed['humidity']
+        condiciones = data_parsed['conditions']
+        min_temp = data_parsed['mint']
+        meteo.append((min_temp,temperatura,precipitacion,humedad,condiciones))
+        return meteo
+    else:
+    # Mostrar un error en caso de que la solicitud haya fallado
+        print("Error:", response.status_code)
+    #print(response.text)
+
     
